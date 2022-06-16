403ce78a233d2c8d617e2cc70c9332478224e052
Pete Hall <pete.mack.hall@gmail.com>
2022-06-16 10:01:49 -0500

    Added HashRouter and set up form routes.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import {Route, HashRouter} from 'react-router-dom';
+
+import Comments from '../Comments/Comments';
+import Feeling from '../Feeling/Feeling';
+import Review from '../Review/Review';
+import Submitted from '../Submitted/Submitted';
+import Support from '../Support/Support';
+import Understanding from '../Understanding/Understanding';
 
 function App() {
 
@@ -9,7 +17,35 @@ function App() {
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
+        <h5>Feeling:</h5>
+        <h5>Understanding:</h5>
+        <h5>Support:</h5>
+        <h5>Comments:</h5>
       </header>
+
+      <HashRouter>
+        <Route path = "/" exact>
+          <Feeling />
+        </Route>
+        <Route path = "/feeling" exact>
+          <Feeling />
+        </Route>
+        <Route path = "/understanding">
+          <Understanding />
+        </Route>
+        <Route path = "/support">
+          <Support />
+        </Route>
+        <Route path = "/comments">
+          <Comments />
+        </Route>
+        <Route path = "/review">
+          <Review />
+        </Route>
+        <Route path = "/submitted">
+          <Submitted />
+        </Route>
+      </HashRouter>
     </div>
   );
 }
