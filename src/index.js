a1b42bf1dff275e915634b491078e69782ab5449
Pete Hall <pete.mack.hall@gmail.com>
2022-06-16 09:52:23 -0500

    set up store and reducers.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const commentsReducer = (state = '', action) => {
+  console.log('in commentsReducer:', action);
+  return state;
+}
+
+const feelingReducer = (state = '', action) => {
+  console.log('in feelingReducer:', action);
+  return state;
+}
+
+const supportReducer = (state = '', action) => {
+  console.log('in supportReducer:', action);
+  return state;
+}
+
+const understandingReducer = (state = '', action) => {
+  console.log('in understandingReducer:', action);
+  return state;
+}
+
+const store = createStore(
+  combineReducers({
+    commentsReducer,
+    feelingReducer,
+    supportReducer,
+    understandingReducer
+  })
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
